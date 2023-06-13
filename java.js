94ab0578646856c18d6f7daa5ceaa7e5ef317675
Eduardo <eduaraujp760@gmail.com>
2023-06-12 22:54:51 -0400

    Atividade: Trabalhando com Variáveis

---
+++ java.js
@@ -0,0 +1,16 @@
+let decimal = 3.14
+console.log(decimal)
+let inteiro = 10
+console.log(inteiro)
+let texto = "namorar"
+console.log(texto)
+let verdade = true
+console.log(verdade)
+let mentira = false
+console.log(mentira)
+let nada = null
+console.log(nada)
+let naoDefinido 
+console.log(naoDefinido)
+
+
